79e3734eefe01bca91212785d6ab7cb5eeb89535
maxtnuk <matnuk@gmail.com>
2018-11-10 22:13:22 +0900

    fix par_iter in determin k

--- eos-ecc/src/signature/ecdsa.rs
+++ eos-ecc/src/signature/ecdsa.rs
@@ -1,202 +1,230 @@
 use prelude::*;
-use signature::ecsignature::EcSignature; 
+use signature::ecsignature::EcSignature;
 use curve::curvetype::EcPoint;
 use curve::curvetype::CurveParam;
-use curve::curvetool::{EcTools};
-use rayon::prelude::*;
+use curve::curvetool::EcTools;
 
 use num_bigint::ToBigInt;
-use num::{BigInt,Integer};
-use num::{Zero,One};
+use num::{BigInt, Integer};
+use num::{Zero, One};
 use key::PublicKey;
 use key::primtool::modinverse;
 
 use std::sync::mpsc::channel;
 use std::thread;
 
-pub fn deterministic_generate_k<F>(curve: &CurveParam,
-                        hashed: Data,
-                        d: BigInt,
-                        mut checksig: F,
-                        nonce: i64) -> Result<BigInt,Errortype>
-where F: FnMut(BigInt) ->bool{
-    let in_hash=
-    if nonce!=0{
-        let zeros=vec![0u8;nonce as usize];
-        let input: Data = hashed.par_iter().chain(zeros.par_iter()).map(|x| *x).collect();
-        hash::sha256(input.as_slice()).to_vec()
-    }else{
+pub fn deterministic_generate_k<F>(
+    curve: &CurveParam,
+    hashed: Data,
+    d: BigInt,
+    mut checksig: F,
+    nonce: i64,
+) -> Result<BigInt, Errortype>
+where
+    F: FnMut(BigInt) -> bool,
+{
+    let in_hash = if nonce != 0 {
+        let zeros = vec![0u8; nonce as usize];
+        let mut hashed = hashed;
+        hashed.extend(zeros);
+        hash::sha256(hashed.as_slice()).to_vec()
+    } else {
         hashed.to_vec()
     };
-    if in_hash.len() != 32{
+    if in_hash.len() != 32 {
         Err(Errortype::WronLength)
-    } else{
-         
-        let x = EcTools::integer_to_vec(d,32);
-        let mut k = vec![0u8;32];
-        let mut v = vec![1u8;32];
-        
+    } else {
+
+        let x = EcTools::integer_to_vec(d, 32);
+        let mut k = vec![0u8; 32];
+        let mut v = vec![1u8; 32];
+
         let mut d_step_buffer = v.clone();
         d_step_buffer.extend_from_slice(&[0u8]);
         d_step_buffer.extend(x.clone());
         d_step_buffer.extend(in_hash.clone());
-        k=hash::hmac_sha256(d_step_buffer.as_slice(),k.as_slice()).to_vec();
-        
-        v=hash::hmac_sha256(v.as_slice(),k.as_slice()).to_vec();
-        
+        k = hash::hmac_sha256(d_step_buffer.as_slice(), k.as_slice()).to_vec();
+
+        v = hash::hmac_sha256(v.as_slice(), k.as_slice()).to_vec();
+
         let mut f_step_buffer = v.clone();
         f_step_buffer.extend_from_slice(&[1u8]);
         f_step_buffer.extend(x.clone());
         f_step_buffer.extend(in_hash.clone());
-        k=hash::hmac_sha256(f_step_buffer.as_slice(),k.as_slice()).to_vec();
-        
-        v=hash::hmac_sha256(v.as_slice(),k.as_slice()).to_vec();
-        v=hash::hmac_sha256(v.as_slice(),k.as_slice()).to_vec();
-        
+        k = hash::hmac_sha256(f_step_buffer.as_slice(), k.as_slice()).to_vec();
+
+        v = hash::hmac_sha256(v.as_slice(), k.as_slice()).to_vec();
+        v = hash::hmac_sha256(v.as_slice(), k.as_slice()).to_vec();
+
         let mut t = EcTools::vec_to_integer(v.clone());
-        
-        while t <= BigInt::zero() || t >= curve.get_n() || !checksig(t.clone()){
-            let mut tmp=v.clone();
+
+        while t <= BigInt::zero() || t >= curve.get_n() || !checksig(t.clone()) {
+            let mut tmp = v.clone();
             tmp.extend_from_slice(&[0]);
-            k = hash::hmac_sha256(tmp.as_slice(),k.as_slice()).to_vec();
-            v = hash::hmac_sha256(v.as_slice(),k.as_slice()).to_vec();
-            
-            v = hash::hmac_sha256(v.as_slice(),k.as_slice()).to_vec();
-            
+            k = hash::hmac_sha256(tmp.as_slice(), k.as_slice()).to_vec();
+            v = hash::hmac_sha256(v.as_slice(), k.as_slice()).to_vec();
+
+            v = hash::hmac_sha256(v.as_slice(), k.as_slice()).to_vec();
+
             t = EcTools::vec_to_integer(v.clone());
         }
         Ok(t)
     }
 }
 
-pub fn sign<T>(curve: &CurveParam,hashed: T,d: BigInt, nonce: i64)->EcSignature
-where T: Into<Data>{
-    let hashed=hashed.into();
+pub fn sign<T>(curve: &CurveParam, hashed: T, d: BigInt, nonce: i64) -> EcSignature
+where
+    T: Into<Data>,
+{
+    let hashed = hashed.into();
     let e = EcTools::vec_to_integer(hashed.clone());
-    let (n,g) = (curve.get_n(),curve.get_g());
-    
+    let (n, g) = (curve.get_n(), curve.get_g());
+
     let mut r = BigInt::default();
     let mut s = BigInt::default();
-    let _k = deterministic_generate_k(curve,hashed,d.clone(),|b|{
-        let q = g.clone().multiply(b.clone());
-        
-        if q.is_infinity() {
-            false
-        }else{
-            r=q.x.unwrap().x.mod_floor(&n);
-            if r == BigInt::zero(){
+    let _k = deterministic_generate_k(
+        curve,
+        hashed,
+        d.clone(),
+        |b| {
+            let q = g.clone().multiply(b.clone());
+
+            if q.is_infinity() {
                 false
-            }else{
-                s = (modinverse(b.clone(),n.clone()).unwrap() * (e.clone() + d.clone() * r.clone())).mod_floor(&n);
-                s != BigInt::zero()
+            } else {
+                r = q.x.unwrap().x.mod_floor(&n);
+                if r == BigInt::zero() {
+                    false
+                } else {
+                    s = (modinverse(b.clone(), n.clone()).unwrap() *
+                             (e.clone() + d.clone() * r.clone()))
+                        .mod_floor(&n);
+                    s != BigInt::zero()
+                }
             }
-        }
-    },nonce);
+        },
+        nonce,
+    );
     //println!("{},{}",r,s);
-    let n_over_two= n.clone() >>1;
-    
-    if s > n_over_two{
-        s= n - s.clone();
+    let n_over_two = n.clone() >> 1;
+
+    if s > n_over_two {
+        s = n - s.clone();
     }
-    EcSignature::new(r,s,Some(curve.curvetype()))
+    EcSignature::new(r, s, Some(curve.curvetype()))
 }
-pub fn verify_raw(curve: &CurveParam,e: BigInt,sig: &EcSignature,q: EcPoint) -> bool{
+pub fn verify_raw(curve: &CurveParam, e: BigInt, sig: &EcSignature, q: EcPoint) -> bool {
     let n = curve.get_n();
     let g = curve.get_g();
-    
+
     let s_clone = sig.s.clone();
     let r_clone = sig.r.clone();
-    
-    let r_flag=r_clone <= BigInt::zero() || r_clone >= n ;
-    let s_flag=s_clone <= BigInt::zero() || s_clone >= n ;
-    match (r_flag,s_flag) {
-        (false,false) =>{
-            let c = modinverse(s_clone.clone(),n.clone()).unwrap();
-            
-            let u1= (e.clone() * c.clone()).mod_floor(&n);
-            let u2= (r_clone.clone() * c.clone()).mod_floor(&n);
-            
-            let r = g.multiply_two(u1,q.clone(),u2);
-            
+
+    let r_flag = r_clone <= BigInt::zero() || r_clone >= n;
+    let s_flag = s_clone <= BigInt::zero() || s_clone >= n;
+    match (r_flag, s_flag) {
+        (false, false) => {
+            let c = modinverse(s_clone.clone(), n.clone()).unwrap();
+
+            let u1 = (e.clone() * c.clone()).mod_floor(&n);
+            let u2 = (r_clone.clone() * c.clone()).mod_floor(&n);
+
+            let r = g.multiply_two(u1, q.clone(), u2);
+
             if r.is_infinity() {
                 false
-            }else{
+            } else {
                 r.x.unwrap().x.mod_floor(&n) == r_clone
             }
         }
-        _=>{
-            false
-        }
+        _ => false,
     }
 }
-pub fn verify(curve: &CurveParam,hashed: Data,sig: &EcSignature,q: EcPoint)->bool{
+pub fn verify(curve: &CurveParam, hashed: Data, sig: &EcSignature, q: EcPoint) -> bool {
     let e = EcTools::vec_to_integer(hashed);
-    verify_raw(curve,e,sig,q)
+    verify_raw(curve, e, sig, q)
 }
-pub fn verify_with_pub(hashed: Data,sig: &EcSignature,pubkey: &PublicKey)->bool{
+pub fn verify_with_pub(hashed: Data, sig: &EcSignature, pubkey: &PublicKey) -> bool {
     let e = EcTools::vec_to_integer(hashed);
-    verify_raw(&pubkey.curveparam,e,sig,pubkey.get_mdata())
+    verify_raw(&pubkey.curveparam, e, sig, pubkey.get_mdata())
 }
 #[allow(non_snake_case)]
-pub fn recover_pubkey(curve: CurveParam,e: BigInt,sig: EcSignature,i: BigInt)->Result<EcPoint,Errortype>{
-    if i.clone() & 3.to_bigint().unwrap()!=i.clone() {
+pub fn recover_pubkey(
+    curve: CurveParam,
+    e: BigInt,
+    sig: EcSignature,
+    i: BigInt,
+) -> Result<EcPoint, Errortype> {
+    if i.clone() & 3.to_bigint().unwrap() != i.clone() {
         Err(Errortype::NotSame)
-    }else{
+    } else {
         let n = curve.get_n();
         let g = curve.get_g();
-        
+
         let s_clone = sig.s.clone();
         let r_clone = sig.r.clone();
-        
-        let r_flag=r_clone <= BigInt::zero() || r_clone >= n ;
-        let s_flag=s_clone <= BigInt::zero() || s_clone >= n ;
-        match (r_flag,s_flag){
-            (false,false) =>{
-              let is_odd = i.clone() & BigInt::one() == BigInt::one();
 
-              let is_second = i.clone() >> 1 == BigInt::one();
-              //need fix
-              let x = if is_second {r_clone.clone()+n.clone()} else {r_clone.clone()};
-              let R = curve.get_curve().new_point_fromx(is_odd, x);
-            
-              let nr = R.clone().multiply(n.clone());
-              if nr.is_infinity(){
-                //println!("nR is not a valid curve point");
-                Err(Errortype::MakeFail{who: "Recovery pubkey".to_string(),
-                                            content: None})  
-              }else{
-                  // Compute -e from e
-                  let eneg={
-                     (-e).mod_floor(&n) 
-                  };
-                
-                  let rinv = modinverse(r_clone.clone(),n.clone()).unwrap();
-                
-                  let q = R.multiply_two(s_clone, g, eneg).multiply(rinv);
-                  if curve.get_curve().validate(&q) {
-                      Ok(q)
-                  }else{
-                      Err(Errortype::MakeFail{who: "Recovery pubkey".to_string(),
-                                                content: None})  
-                  }
-              }
-            },
-            _ =>{
-              Err(Errortype::MakeFail{who: "Recovery pubkey".to_string(),
-                                            content: None})  
+        let r_flag = r_clone <= BigInt::zero() || r_clone >= n;
+        let s_flag = s_clone <= BigInt::zero() || s_clone >= n;
+        match (r_flag, s_flag) {
+            (false, false) => {
+                let is_odd = i.clone() & BigInt::one() == BigInt::one();
+
+                let is_second = i.clone() >> 1 == BigInt::one();
+                //need fix
+                let x = if is_second {
+                    r_clone.clone() + n.clone()
+                } else {
+                    r_clone.clone()
+                };
+                let R = curve.get_curve().new_point_fromx(is_odd, x);
+
+                let nr = R.clone().multiply(n.clone());
+                if nr.is_infinity() {
+                    //println!("nR is not a valid curve point");
+                    Err(Errortype::MakeFail {
+                        who: "Recovery pubkey".to_string(),
+                        content: None,
+                    })
+                } else {
+                    // Compute -e from e
+                    let eneg = {
+                        (-e).mod_floor(&n)
+                    };
+
+                    let rinv = modinverse(r_clone.clone(), n.clone()).unwrap();
+
+                    let q = R.multiply_two(s_clone, g, eneg).multiply(rinv);
+                    if curve.get_curve().validate(&q) {
+                        Ok(q)
+                    } else {
+                        Err(Errortype::MakeFail {
+                            who: "Recovery pubkey".to_string(),
+                            content: None,
+                        })
+                    }
+                }
+            }
+            _ => {
+                Err(Errortype::MakeFail {
+                    who: "Recovery pubkey".to_string(),
+                    content: None,
+                })
             }
         }
     }
 }
-pub fn calc_pubkey_recovery_param(curve: &CurveParam,
-                                  e: BigInt,
-                                  sig: &EcSignature,
-                                  pubkey: &PublicKey)->Result<u32,Errortype>{
+pub fn calc_pubkey_recovery_param(
+    curve: &CurveParam,
+    e: BigInt,
+    sig: &EcSignature,
+    pubkey: &PublicKey,
+) -> Result<u32, Errortype> {
 
-    let not_find=5;
+    let not_find = 5;
 
     let (sender, receiver) = channel();
-    
+
     let handles: Vec<_> = (0..4).map(|idx| {
         let tx=sender.clone();
         let t_curve=curve.clone();
@@ -205,9 +233,9 @@ pub fn calc_pubkey_recovery_param(curve: &CurveParam,
         let t_e=e.clone();
         thread::spawn(move || {
             //println!("current i: {}",idx);
-           
+
             let q_prime=recover_pubkey(t_curve,t_e,t_sig,idx.to_bigint().unwrap());
-            
+
             let return_val: u32=match q_prime{
                 Ok(val)=>{
                     //println!("val \nx: {}\n y:{}",val.clone().x.unwrap(),val.clone().y.unwrap());
@@ -226,15 +254,15 @@ pub fn calc_pubkey_recovery_param(curve: &CurveParam,
             tx.send(return_val).unwrap();
         })
     }).collect();
-    
+
     let mut result = Err(Errortype::NotFind);
     let mut idxs = Vec::with_capacity(4);
     for _ in 0..4 {
         idxs.push(receiver.recv().unwrap());
     }
-    for i in idxs.iter(){
+    for i in idxs.iter() {
         if not_find != *i {
-            result=Ok(*i);
+            result = Ok(*i);
             break;
         }
     }
@@ -248,7 +276,7 @@ pub fn calc_pubkey_recovery_param(curve: &CurveParam,
     (0..4 as u32).into_par_iter().for_each_with(sender, |s, x: u32| {
         println!("current i: {}",x);
         let q_prime=recover_pubkey(curve,e.clone(),sig,x.to_bigint().unwrap());
-        
+
         let return_val: u32=match q_prime{
             Ok(val)=>{
                 println!("val \nx: {}\n y:{}",val.clone().x.unwrap(),val.clone().y.unwrap());
--- eos-ecc/src/signature/mod.rs
+++ eos-ecc/src/signature/mod.rs
@@ -3,8 +3,8 @@ pub mod ecsignature;
 pub mod ecdsa;
 
 use prelude::*;
-use num::{BigInt,ToPrimitive};
-use key::{PublicKey,PrivateKey};
+use num::{BigInt, ToPrimitive};
+use key::{PublicKey, PrivateKey};
 
 use curve::curvetool::CurveType;
 use self::ecsignature::EcSignature;
@@ -13,115 +13,108 @@ use curve::curvetool::EcTools;
 
 use key::keyutil;
 
-pub struct Signature{
+pub struct Signature {
     i: BigInt,
-    sig: EcSignature
+    sig: EcSignature,
 }
-impl Signature{
-    fn new(r: BigInt,
-           s: BigInt,
-           i: BigInt,
-           ctype: Option<CurveType>)->Self{
-        Signature{
-            i:i,
-            sig: EcSignature::new(r,s,ctype)
-        }           
-    }
-    fn new_raw(sig: EcSignature,i :BigInt)->Self{
-        Signature{
+impl Signature {
+    fn new(r: BigInt, s: BigInt, i: BigInt, ctype: Option<CurveType>) -> Self {
+        Signature {
             i: i,
-            sig: sig
+            sig: EcSignature::new(r, s, ctype),
         }
     }
-    pub fn get_ecsig(&self) -> EcSignature{
+    fn new_raw(sig: EcSignature, i: BigInt) -> Self {
+        Signature { i: i, sig: sig }
+    }
+    pub fn get_ecsig(&self) -> EcSignature {
         self.sig.clone()
     }
-    pub fn verify(sig: &EcSignature,data: Data,pubkey: &PublicKey)->bool{
-        let hashed= hash::sha256(data.as_slice()).to_vec();
-        Self::verifyhash(sig,hashed,pubkey)
+    pub fn verify(sig: &EcSignature, data: Data, pubkey: &PublicKey) -> bool {
+        let hashed = hash::sha256(data.as_slice()).to_vec();
+        Self::verifyhash(sig, hashed, pubkey)
     }
-    pub fn verifyhash(sig: &EcSignature,hashed: Data,pubkey: &PublicKey)->bool{
-        ecdsa::verify_with_pub(hashed,sig,&pubkey)
+    pub fn verifyhash(sig: &EcSignature, hashed: Data, pubkey: &PublicKey) -> bool {
+        ecdsa::verify_with_pub(hashed, sig, &pubkey)
     }
-    fn recover(&self,data: Data)->Result<PublicKey,Errortype>{
-        let hashed= hash::sha256(data.as_slice()).to_vec();
+    fn recover(&self, data: Data) -> Result<PublicKey, Errortype> {
+        let hashed = hash::sha256(data.as_slice()).to_vec();
         self.recoverhash(hashed)
     }
-    
-    fn recoverhash(&self,hashed: Data)->Result<PublicKey,Errortype>{
+
+    fn recoverhash(&self, hashed: Data) -> Result<PublicKey, Errortype> {
         let e = EcTools::vec_to_integer(hashed);
-        
-        let mut i2=self.i.clone() - 27.to_bigint().unwrap();
+
+        let mut i2 = self.i.clone() - 27.to_bigint().unwrap();
         i2 &= 3.to_bigint().unwrap();
-        
-        match ecdsa::recover_pubkey(self.sig.curve.clone(),e,self.sig.clone(),i2){
-            Ok(val) =>{
-                Ok(PublicKey::new_raw(self.sig.curve.clone(),val))
-            },
-            Err(err)=>{
+
+        match ecdsa::recover_pubkey(self.sig.curve.clone(), e, self.sig.clone(), i2) {
+            Ok(val) => Ok(PublicKey::new_raw(self.sig.curve.clone(), val)),
+            Err(err) => {
                 err_print(err);
-                Err(Errortype::MakeFail{who: "PublicKey".to_string(),
-                                        content: None})
+                Err(Errortype::MakeFail {
+                    who: "PublicKey".to_string(),
+                    content: None,
+                })
             }
         }
     }
-    fn to_vec(&self) -> Data{
-        let i_u8=self.i.to_u8().unwrap();
+    fn to_vec(&self) -> Data {
+        let i_u8 = self.i.to_u8().unwrap();
 
-        let vec_r=EcTools::integer_to_vec(self.sig.r.clone(),32);
-        let vec_s=EcTools::integer_to_vec(self.sig.s.clone(),32);
-        
-        let mut before_data:Data = vec_r.iter().chain(vec_s.iter()).map(|x| *x).collect();
-        before_data.insert(0,i_u8);
+        let vec_r = EcTools::integer_to_vec(self.sig.r.clone(), 32);
+        let vec_s = EcTools::integer_to_vec(self.sig.s.clone(), 32);
+
+        let mut before_data: Data = vec_r.iter().chain(vec_s.iter()).map(|x| *x).collect();
+        before_data.insert(0, i_u8);
         before_data
     }
-    pub fn sign<T>(data: T,pvkey: &PrivateKey)->Self
-    where T: Into<Data>{
-        let data= data.into();
-        let hashed= hash::sha256(data.as_slice());
-        Self::signhash(hashed,pvkey)
+    pub fn sign<T>(data: T, pvkey: &PrivateKey) -> Self
+    where
+        T: Into<Data>,
+    {
+        let data = data.into();
+        let hashed = hash::sha256(data.as_slice());
+        Self::signhash(hashed, pvkey)
     }
-    pub fn signhash<T>(hashed: T,pvkey: &PrivateKey)->Self
-    where T: Into<Data>{
-        let hashed=hashed.into();
-        let mut nonce=0;
+    pub fn signhash<T>(hashed: T, pvkey: &PrivateKey) -> Self
+    where
+        T: Into<Data>,
+    {
+        let hashed = hashed.into();
+        let mut nonce = 0;
         let e = EcTools::vec_to_integer(hashed.clone());
 
-        let curve=pvkey.curveparam.clone();
-        let pubkey=PublicKey::from(pvkey);
-        let (i,ecdsig) =loop {
-            if nonce % 10 == 0{
-                println!("WARN: {} attempts to find canonical signature",nonce);
+        let curve = pvkey.curveparam.clone();
+        let pubkey = PublicKey::from(pvkey);
+        let (i, ecdsig) = loop {
+            if nonce % 10 == 0 {
+                println!("WARN: {} attempts to find canonical signature", nonce);
             }
-            println!("current nonce: {}",nonce);
-            let in_ecdsig = ecdsa::sign(&curve,
-                                         hashed.clone(),
-                                         pvkey.get_mdata(),
-                                         nonce);
-            nonce+=1;
+            println!("current nonce: {}", nonce);
+            let in_ecdsig = ecdsa::sign(&curve, hashed.clone(), pvkey.get_mdata(), nonce);
+            nonce += 1;
             let der = in_ecdsig.to_der();
             let len_r = der[3];
-            let len_s = der[5+len_r as usize];
-            if len_r == 32 && len_s ==32{
-                match ecdsa::calc_pubkey_recovery_param(&curve,
-                                                     e.clone(),
-                                                     &in_ecdsig,
-                                                     &pubkey){
-                    Ok(val) =>{
-                        break (val+31,in_ecdsig);
-                    },
-                    Err(err)=>{
+            let len_s = der[5 + len_r as usize];
+            if len_r == 32 && len_s == 32 {
+                match ecdsa::calc_pubkey_recovery_param(&curve, e.clone(), &in_ecdsig, &pubkey) {
+                    Ok(val) => {
+                        break (val + 31, in_ecdsig);
+                    }
+                    Err(err) => {
                         err_print(err);
                         continue;
                     }
-                } 
+                }
             }
         };
-        Signature::new_raw(ecdsig,i.to_bigint().unwrap())
+        Signature::new_raw(ecdsig, i.to_bigint().unwrap())
     }
 }
-impl ToString for Signature{
-    fn to_string(&self)->String{
-        "SIG_K1_".to_string() + keyutil::check_encode(self.to_vec(),Some("K1".to_string())).as_str()
+impl ToString for Signature {
+    fn to_string(&self) -> String {
+        "SIG_K1_".to_string() +
+            keyutil::check_encode(self.to_vec(), Some("K1".to_string())).as_str()
     }
 }
